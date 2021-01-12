1ad84979e6a390b8e3bdb38716ba7a3ea2750096
DiatomicOxygen <>
2021-01-12 13:18:12 -0500

    length method works

--- Radix.java
+++ Radix.java
@@ -1,7 +1,12 @@
 public class Radix {
 
   public static int nth(int n, int col) {
-    return (n / ((int)Math.pow(10, col))) % 10;
+    int num = n;
+    return (num / ((int)Math.pow(10, col))) % 10;
   }
 
+  public static int length(int n) {
+    int num = n;
+    return (int)(Math.log((double)num) / Math.log(10.0));
+  }
 }
